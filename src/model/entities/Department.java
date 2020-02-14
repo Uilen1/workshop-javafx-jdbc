2c72b83419cfeb2a13e7ccac17ac6908489235b4
Uilen Lelles <uilenlelles@hotmail.com>
2020-02-14 17:09:48 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -8,13 +8,11 @@ public class Department implements Serializable {
 
 	private Integer id;
 	private String name;
-
+	
 	public Department() {
-
 	}
 
 	public Department(Integer id, String name) {
-
 		this.id = id;
 		this.name = name;
 	}
@@ -64,5 +62,4 @@ public class Department implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
 }
